62ecfe0b0f6e9e00005ad0a5a2e13e0501c62599
thomasyang <thomasyang@ixensor.com>
2022-02-05 19:20:14 +0800

    lesson 1

---
+++ 01_05_introduce_pytorch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  5 18:54:27 2022
+
+@author: SuTungYang
+"""
+
+import torch
+
+# print(torch.cuda.is_available())
+print(dir(torch))
+print("="*80)
+print(dir(torch.cuda))
+print("="*80)
+help(torch.cuda.is_available)
